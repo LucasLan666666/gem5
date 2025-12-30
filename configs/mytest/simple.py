2fe12f0142e2e0e4611d86bc0c851f9ce48344c5
Lan Yuzhou <lanyuzhou22@mails.ucas.ac.cn>
2025-12-30 15:46:43 +0800

    misc: Clean up commented-out DRAMsim3 configuration in simple.py

--- configs/mytest/simple.py
+++ configs/mytest/simple.py
@@ -81,11 +81,9 @@ system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
 # Create a DDR3 memory controller and connect it to the membus
 system.mem_ctrl = MemCtrl()
 system.mem_ctrl.dram = DDR3_1600_8x8()
+# system.mem_ctrl = DRAMsim3()
 system.mem_ctrl.dram.range = system.mem_ranges[0]
 system.mem_ctrl.port = system.membus.mem_side_ports
-# system.mem_ctrl = DRAMsim3()
-# system.mem_ctrl.range = system.mem_ranges[0]
-# system.mem_ctrl.port = system.membus.mem_side_ports
 
 # Connect the system up to the membus
 system.system_port = system.membus.cpu_side_ports
